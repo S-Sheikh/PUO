93e39e951de1b870f06b41f5f42b4f471ba27c8e
Shahbaaz <shabibo241995@gmail.com>
2016-09-05 16:04:27 +0200

    Word Game Home Layout ! No Pics as yet , no intention of adding pics , just want functionality ; will make own icons at home

---
+++ app/src/main/java/za/ac/cut/puo/GameHome.java
@@ -0,0 +1,13 @@
+package za.ac.cut.puo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_home);
+    }
+}
