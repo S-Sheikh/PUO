4eeb9c01097c80168097f3177a81db2d9b0b5867
Shahbaaz <shabibo241995@gmail.com>
2016-09-06 12:17:50 +0200

    Tweaks to Game Headers

---
+++ app/src/main/java/za/ac/cut/puo/GameMatchDefinition.java
@@ -0,0 +1,13 @@
+package za.ac.cut.puo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameMatchDefinition extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_match_definition);
+    }
+}
