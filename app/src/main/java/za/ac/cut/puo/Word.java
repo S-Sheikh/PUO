e750aa325999a8a24032fbd513a50d30b8ff9899
hodiel <siyabonga.nano@gmail.com>
2016-09-13 14:06:36 +0200

    Fixed Repo...

--- app/src/main/java/za/ac/cut/puo/Word.java
+++ app/src/main/java/za/ac/cut/puo/Word.java
@@ -1,16 +1,62 @@
 package za.ac.cut.puo;
 
-import java.util.Date;
-
-
+/**
+ * Created by Shahbaaz Sheikh on 12/09/2016.
+ */
 public class Word {
-    private String word;
-    private String wordType;
-    private String language;
-    private String userMail;
-    private boolean wordSupported;
-    private String ratingBarCount;
-    private String objectId;
-    private Date created;
-    private Date updated;
+    private String Word;
+    private String Language;
+    private String Definition;
+    private String PartOfSpeech;
+    boolean repeatFlag;
+
+    public boolean isRepeatFlag() {
+        return repeatFlag;
+    }
+
+    public void setRepeatFlag(boolean repeatFlag) {
+        this.repeatFlag = repeatFlag;
+    }
+
+    public Word(String word, String language, String definition, String partOfSpeech) {
+        Word = word;
+        Language = language;
+        Definition = definition;
+
+        PartOfSpeech = partOfSpeech;
+        repeatFlag = false;
+    }
+
+    public String getLanguage() {
+        return Language;
+    }
+
+    public void setLanguage(String language) {
+        Language = language;
+    }
+
+    public String getWord() {
+        return Word;
+    }
+
+    public void setWord(String word) {
+        Word = word;
+    }
+
+    public String getDefinition() {
+        return Definition;
+    }
+
+    public void setDefinition(String definition) {
+        Definition = definition;
+    }
+
+    public String getPartOfSpeech() {
+        return PartOfSpeech;
+    }
+
+    public void setPartOfSpeech(String partOfSpeech) {
+        PartOfSpeech = partOfSpeech;
+    }
+
 }
