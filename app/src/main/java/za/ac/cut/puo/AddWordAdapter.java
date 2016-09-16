0a12b9df12811033d7c55135c1b25f7d72333b97
Watley <watlinton@hotmail.com>
2016-09-16 08:57:03 +0200

    HomeScreen Menu Items Layout changed :D

--- app/src/main/java/za/ac/cut/puo/AddWordAdapter.java
+++ app/src/main/java/za/ac/cut/puo/AddWordAdapter.java
@@ -6,17 +6,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AddWordAdapter extends ArrayAdapter<AddWord> {
     private final Context context;
     private final List<AddWord> values;
     TextView tv_word_title, tv_word_status, tv_word_author, tvLanguage;
+    ImageView wordDescImg;
 
     public AddWordAdapter(Context context, List<AddWord> list) {
-        super(context, R.layout.home_word_row, list);
+        super(context, R.layout.word_list_item, list);
         this.context = context;
         this.values = list;
     }
@@ -24,13 +28,14 @@ public class AddWordAdapter extends ArrayAdapter<AddWord> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.home_word_row, parent, false);
-        tv_word_title = (TextView) rowView.findViewById(R.id.tvWord);
-        tv_word_status = (TextView) rowView.findViewById(R.id.tvStatus);
-        tv_word_author = (TextView) rowView.findViewById(R.id.tvAuthor);
-        tvLanguage = (TextView) rowView.findViewById(R.id.tvLanguage);
+        View rowView = inflater.inflate(R.layout.word_list_item, parent, false);
+        wordDescImg = (CircleImageView) rowView.findViewById(R.id.civ_word_pic);
+        tv_word_title = (TextView) rowView.findViewById(R.id.tv_word_text);
+        tv_word_status = (TextView) rowView.findViewById(R.id.tv_word_status);
+        tv_word_author = (TextView) rowView.findViewById(R.id.tv_word_author);
+        tvLanguage = (TextView) rowView.findViewById(R.id.tv_word_lexicon);
         tv_word_title.setText(values.get(position).getWord());
-        tvLanguage.setText(values.get(position).getLanguage());
+        tvLanguage.setText(values.get(position).getLanguage() + "," + values.get(position).getPartOfSpeech());
         if (values.get(position).isSupported()) {
             tv_word_status.setText(getContext().getString(R.string.supported));
         } else {
